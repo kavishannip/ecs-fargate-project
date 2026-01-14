943c38ea6aa0ca6330526a414cbdf12bd8364672
M D J Kavishan Nipun Shantha <115109229+kavishannip@users.noreply.github.com>
2026-01-14 22:29:07 +0530

    update workflow

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -109,7 +109,7 @@ resource "aws_iam_role" "github_actions" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:YOUR_GITHUB_USERNAME/YOUR_REPO_NAME:*"
+            "token.actions.githubusercontent.com:sub" = "repo:kavishannip/ecs-fargate-project:*"
           }
         }
       }
